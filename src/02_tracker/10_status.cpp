4b569352a7d4e2a741843ba7243a44ee9b8093f9
zijianye <zijianye96@gmail.com>
2024-03-16 23:37:53 +0800

    finish:状态检查类cpp

--- src/02_tracker/10_status.cpp
+++ src/02_tracker/10_status.cpp
@@ -17,13 +17,20 @@ void status_c::stop()
 // 线程执行过程
 void *status_c::run()
 {
+  time_t last = time(NULL);
   while (!m_stop)
   {
+    sleep(1);
     // 现在时间
-
+    time_t now = time(NULL);
     // 若现在距离最近一次检查存储服务器状态已足够久
-    // 检查存储服务器的状态
-    // 更新最近一次检查时间
+    if (now - last >= cfg_interval)
+    {
+      // 检查存储服务器的状态
+      check();
+      // 更新最近一次检查时间
+      last = now;
+    }
   }
 
   return nullptr;
@@ -31,6 +38,35 @@ void *status_c::run()
 // 检查存储服务器的状态
 int status_c::check() const
 {
-
+  // 现在
+  time_t now = time(NULL);
+  // 检查最后一次心跳时间
+  // 互斥锁加锁
+  if ((errno = pthread_mutex_lock(&g_mutex)))
+  {
+    logger_error("call pthread_mutex_lock fail: %s",
+                 strerror(errno));
+    return ERROR;
+  }
+  // 遍历组表中的每一个组
+  for (std::map<std::string, std::list<storage_info_t>>::iterator group = g_groups.begin(); group != g_groups.end(); ++group)
+  {
+    // 遍历该组中每一台存储服务器
+    for (std::list<storage_info_t>::iterator si = group->second.begin(); si != group->second.end(); ++si)
+    {
+      if (now - si->si_btime >= cfg_interval)
+      {
+        // 将其标记为离线
+        si->si_status = STORAGE_STATUS_OFFLINE;
+      }
+    }
+  }
+  // 互斥锁解锁
+  if ((errno = pthread_mutex_unlock(&g_mutex)))
+  {
+    logger_error("call pthread_mutex_unlock fail: %s",
+                 strerror(errno));
+    return ERROR;
+  }
   return OK;
 }
